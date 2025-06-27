6d977ce5c295775c6c1306aee7c280640f6db7ef
Aneesh-Deshpande123 <aneeshad20@gmail.com>
2025-06-26 17:52:25 -0700

    Updated html repo

---
+++ 3.js
@@ -0,0 +1,14 @@
+let bulb1=document.getElementById("bulb1_id");
+let bulb2=document.getElementById("bulb2_id");
+let bulbOn=true;
+bulb1.onclick=function event(){
+    if(bulbOn==true){
+        bulbOn=false;
+        bulb1.src="bulboff.gif";
+        bulb2.textContent="Bulb is off";
+    }else{
+        bulbOn=true;
+        bulb1.src="bulbon.gif";
+        bulb2.textContent="Bulb is on";
+    }
+}
